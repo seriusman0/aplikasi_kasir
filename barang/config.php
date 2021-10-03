6a604ec49a29ba17889a1d5d6d0fc24ca4603756
Seriusman Waruwu <seriusman0@gmail.com>
2021-10-03 22:40:07 +0700

    config doang

--- barang/config.php
+++ barang/config.php
@@ -1,7 +1,7 @@
 <?php
 $server = "localhost";
 $username = "root";
-$password = "";
+$password = "tmDevFlats0987^(";
 $database = "kasir";
 
 $conn = mysqli_connect($server, $username, $password, $database) or die("Gagal");
